97f0695236725cf65d526eea194bf4955875daf9
Rishav Madnavat <rishav.madnavat@tekmindz.com>
2024-10-23 12:20:37 +0530

    Intial commit of service I

---
+++ servicei/src/main/java/com/jeriv/servicei/ServiceiApplication.java
@@ -0,0 +1,13 @@
+package com.jeriv.servicei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceiApplication.class, args);
+	}
+
+}
---
+++ servicei/src/test/java/com/jeriv/servicei/ServiceiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeriv.servicei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
